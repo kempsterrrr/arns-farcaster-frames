b0501ff1239e4249c90c93e206de7592950e241d
kempsterrrr <kempsterwilliam@gmail.com>
2025-02-08 22:36:37 +0000

    adding static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: "./",
+  output: "export", // Enables static HTML export
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
